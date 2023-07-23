1a369cd076a085a3c6caa5345c15d9d6fb28a13a
Mohamed Khaled <mohammdk393@gmail.com>
2023-07-24 02:46:25 +0300

    upload project

---
+++ compositional Layout/Cell/HeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderView.swift
+//  compositional Layout
+//
+//  Created by Mohamed Khaled on 23/07/2023.
+//
+
+import UIKit
+
+class HeaderView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
---
+++ compositional Layout/Helper.swift
@@ -0,0 +1,11 @@
+//
+//  Helper.swift
+//  compositional Layout
+//
+//  Created by Mohamed Khaled on 22/07/2023.
+//
+
+import Foundation
+import UIKit
+
+typealias collectionViewType = UICollectionViewDelegate & UICollectionViewDataSource
